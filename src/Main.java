5d2093de37e5811c5d9648a4e4ff2abf9c14a72a
Yackov <yackov.irkyt@gmail.com>
2023-04-06 02:17:33 +0500

    1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int topup = 100;
-
-        System.out.println("Hello world!");
+        int check = 100;
+        int topup = 1234;
+        int bonus;
+        if (topup > 1000) {
+            bonus = topup / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = check + topup;
+        System.out.println("Ваш счет: " + balance);
+        System.out.println("Ваши бонусы: " + bonus);
     }
 }
